7d40bea24df390308d75f1682fb3cdd7a154f071
odesigo <chidera@winexviv.com>
2024-10-26 19:48:31 +0100

    stable commit

---
+++ INSTALL.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Installing Tools and Modules....
+pip install requests
+pip install requests[socks]
+pip install colorama
+pip install httpx
+pip install httpx_socks
+pip install aiofiles
+pip install aiosqlite
+pip install anyio
+pip install fake-useragent
+pause
