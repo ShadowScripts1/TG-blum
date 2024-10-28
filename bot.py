732b14e5257cda1230005c3ee8010af590f5f6df
odesigo <chidera@winexviv.com>
2024-10-28 15:30:08 +0100

    fix up

--- bot.py
+++ bot.py
@@ -557,8 +557,8 @@ async def get_data(data_file, proxy_file):
 async def main():
     banner = f"""
 {magenta} _____ __    _____ _____  {white}BLUM AUTO TASK AND PLAY GAME BOT {green}BLUM
-{magenta}| __  |  |  |  |  |     | {green}Author : {white}D4rkCipherX
-{magenta}| __ -|  |__|  |  | | | | {white}YouTube : {green}https://youtube.com/@d4rkcipherx
+{magenta}| __  |  |  |  |  |     | {green}Author : {white}Shadow Scripter
+{magenta}| __ -|  |__|  |  | | | | {white}YouTube : {green}https://www.youtube.com/@ShadowScripts1
 {magenta}|_____|_____|_____|_|_|_| {green}Note : {white}MUST SUBSCRIBE MY YOUTUBE CHANNEL
         """
     arg = argparse.ArgumentParser()
